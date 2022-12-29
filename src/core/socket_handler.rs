d8bae3e6a6ed536d53f4e38f41d8d4f3b1dc3ac3
Keith Valin <keith@mailtux.net>
2022-12-29 18:34:10 -0500

    Added documentation for functions in socket handler

--- src/core/socket_handler.rs
+++ src/core/socket_handler.rs
@@ -1,6 +1,5 @@
 use log::{debug, error, warn};
 
-use tokio::sync::oneshot::Sender;
 use interprocess::local_socket::{
     NameTypeSupport, 
     tokio::{LocalSocketListener, LocalSocketStream}
@@ -15,20 +14,21 @@ pub struct SocketHandler {
 
 
 impl SocketHandler {
-    /// Creates a new SocketHandler and listens either on a namespaced local socket
-    /// or a file path socket.
-    /// 
-    /// # Arguments
-    /// ## `socket_name`
-    /// The name to be used for the filepath or namespace
-    /// 
-    /// # Returns
-    /// A `Result` is returned, if successful the SocketHandler is provided
-    /// otherwise, a `String` is returned containing the error.
-    /// 
-    /// # Platform-Dependent Behavior
-    /// * Windows/Linux - Creates a namespaced socket (@[`socket_name`](#socket_name).sock)
-    /// * BSD/Mac/*NIX - Creates a filepath socket at /tmp/[`socket_name`](#socket_name).sock
+    /** Creates a new SocketHandler and listens either on a namespaced local socket
+     * or a file path socket.
+     * 
+     * # Arguments
+     * ## `socket_name`
+     * The name to be used for the filepath or namespace
+     * 
+     * # Returns
+     * A `Result` is returned, if successful the SocketHandler is provided
+     * otherwise, a `String` is returned containing the error.
+     * 
+     * # Platform-Dependent Behavior
+     * - Windows/Linux - Creates a namespaced socket (@[`socket_name`](#socket_name).sock)
+     * - BSD/Mac/\*NIX - Creates a filepath socket at /tmp/[`socket_name`](#socket_name).sock
+     **/
     pub fn new(socket_name: &str) -> Result<SocketHandler, String> {
         let name = {
             use NameTypeSupport::*;
@@ -52,11 +52,12 @@ impl SocketHandler {
         })
     }
 
-    ///
-    pub async fn run(&self, notify: Sender<()>) {
-        let _ = notify.send(());
-        
-
+    /** The main thread loop of SocketHandler, this will handle the
+     * incoming socket connections and direct them off to where they need to go
+     * 
+     * **THIS FUNCTION CONTAINS AN INFINITE LOOP, RUN IT IN ITS OWN THREAD**
+     */
+    pub async fn run(&self) {      
         loop {
             let conn = match self.listener.accept().await {
                 Ok(c) => c,
@@ -66,11 +67,29 @@ impl SocketHandler {
                 }
             };
 
-            self.handle_connection(conn).await;
+            let data = match self.recv_data(conn).await {
+                None => {
+                    continue;
+                },
+                Some(s) => s
+            };
+
         }
     }
-    async fn handle_connection(&self, conn: LocalSocketStream) {
-        let (reader, mut writer) = conn.into_split();
+
+    /** Receives data from a new connection, returning any data it might have sent
+     * 
+     * # Arguments
+     * ## `conn`
+     * A LocalSocketStream connection to a remote process
+     * 
+     * # Returns
+     * `None` if no data was received (or the read errored out)
+     * 
+     * A `String` containing the data sent if the connection suceeded.
+     */
+    async fn recv_data(&self, conn: LocalSocketStream) -> Option<String> {
+        let (reader, _) = conn.into_split();
         let mut reader = BufReader::new(reader);
         let mut data = String::with_capacity(128);
 
@@ -80,10 +99,11 @@ impl SocketHandler {
             Ok(size) => {
                 debug!("Received {} bytes from a client", size);
                 debug!("Message contents: {}", data);
+                Some(data)
             },
             Err(e) => {
                 warn!("Could not read from client: {}", e);
-                return;
+                return None;
             }
         }
     }
