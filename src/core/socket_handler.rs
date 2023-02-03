429320ad5b1692c9cb4afb6f25d0ea6f190e895e
Keith Valin <keith@mailtux.net>
2023-02-03 11:30:33 -0500

    Added some unit tests to sockethandler

--- src/core/socket_handler.rs
+++ src/core/socket_handler.rs
@@ -2,7 +2,7 @@ use crate::api::schema::{
     instructions::{CoreInstruction, CoreInstructionType}
 };
 
-use log::{debug, error, warn};
+use log::{debug, error, warn, trace};
 use interprocess::local_socket::{
     NameTypeSupport, 
     tokio::{LocalSocketListener, LocalSocketStream}
@@ -10,8 +10,11 @@ use interprocess::local_socket::{
 use futures::{
     io::BufReader, AsyncBufReadExt
 };
+use std::{path::Path, fs};
 
+#[derive(Debug)]
 pub struct SocketHandler {
+    socket_name: String,
     listener: LocalSocketListener
 }
 
@@ -32,14 +35,8 @@ impl SocketHandler {
      * - Windows/Linux - Creates a namespaced socket (@[`socket_name`](#socket_name).sock)
      * - BSD/Mac/\*NIX - Creates a filepath socket at /tmp/[`socket_name`](#socket_name).sock
      **/
-    pub fn new(socket_name: &str) -> Result<Self, String> {
-        let name = {
-            use NameTypeSupport::*;
-            match NameTypeSupport::query() {
-                OnlyPaths | Both => format!("/tmp/{}.sock", socket_name),
-                OnlyNamespaced => format!("@{}.sock", socket_name),
-            }
-        };
+    pub fn new<S>(socket_name: S) -> Result<Self, String> where S: Into<String> + std::fmt::Display {
+        let name = get_socket_name(socket_name);
 
         debug!("Attempting to start server at {}", name);
         let listener = match LocalSocketListener::bind(name.clone()) {
@@ -51,7 +48,8 @@ impl SocketHandler {
         };
         debug!("Server started at {}", name);
         Ok(SocketHandler{
-            listener
+            listener,
+            socket_name: name
         })
     }
 
@@ -128,6 +126,7 @@ impl SocketHandler {
      * A String containing error information on failure
      */
     async fn handle_message(&self, data: String) -> Result<CoreInstruction, String> {
+        trace!("Serializing {}", data);
         let data = match serde_json::from_str::<CoreInstruction>(data.as_str()) {
             Ok(data) => data,
             Err(e) => {
@@ -150,4 +149,85 @@ impl SocketHandler {
 
         Ok(data)
     }
+}
+
+impl Drop for SocketHandler {
+    fn drop(&mut self) {
+        use NameTypeSupport::*;
+        match NameTypeSupport::query() {
+            OnlyPaths | Both => {
+                let path = Path::new(&self.socket_name);
+                if path.exists() {
+                    let res = fs::remove_file(path);
+                    match res {
+                        Ok(_) => {
+                            debug!("Socket successfully removed");
+                        },
+                        Err(e) => {
+                            error!("Could not clean up socket: {}", e.to_string());
+                        }
+                    }
+                }
+            },
+            OnlyNamespaced => {},
+        }
+    }
+}
+
+fn get_socket_name<S>(name: S) -> String where S: Into<String> + std::fmt::Display {
+    use NameTypeSupport::*;
+    match NameTypeSupport::query() {
+        OnlyPaths | Both => format!("/tmp/{}.sock", name),
+        OnlyNamespaced => format!("@{}.sock", name),
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use crate::core::SocketHandler;
+    use rstest::*;
+
+    #[tokio::test]
+    #[ignore = "Single Threaded test"]
+    async fn create_socket_succeeds() {
+        let socket = SocketHandler::new("polychat");
+
+        assert!(socket.is_ok(), "Socket Handler was unable to init: {}", socket.unwrap_err());
+    }
+
+    #[tokio::test]
+    #[ignore = "Single Threaded test"]
+    async fn socket_cleans_up_after_itself() {
+        let socket = SocketHandler::new("polychat");
+
+        assert!(socket.is_ok(), "Second SocketHandler could not be initialized: {}", socket.unwrap_err());
+    }
+
+    #[tokio::test]
+    async fn socket_json_handles_malformed_instruction() {
+        let socket = SocketHandler::new("malformed_instruction");
+        let garbage = "{\"instruction_type\": \"Silliness\",\"payload\": {}}";
+
+        assert!(socket.is_ok(), "Could not init SocketHandler");
+        let socket = socket.unwrap();
+
+        let ins = socket.handle_message(String::from(garbage)).await;
+        assert!(ins.is_err(), "SocketHandler did not err on malformed instruction");
+    }
+
+    #[rstest]
+    #[case("Init")]
+    #[case("KeepaliveResponse")]
+    #[case("AuthAccountResponse")]
+    #[tokio::test]
+    async fn socket_json_handles_valid_core_instruction_types(#[case] ins_type: String) {
+        let socket = SocketHandler::new(format!("{}_instruction", ins_type));
+        assert!(socket.is_ok(), "Could not init SocketHandler");
+        let socket = socket.unwrap();
+        
+        let inst = format!("{{\"instruction_type\": \"{}\", \"payload\": {{}} }}", ins_type);
+        let result = socket.handle_message(String::from(inst)).await;
+        assert!(result.is_ok(), "SocketHandler could not handle type: {}", ins_type);
+    }
+
 }
