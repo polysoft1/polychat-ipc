8b652c4128540a4ba71930426bc0933d604877bd
Keith Valin <keith@mailtux.net>
2023-02-25 12:38:54 -0500

    Initial threading on socket communicating

--- src/process_management/process.rs
+++ src/process_management/process.rs
@@ -1,31 +1,54 @@
-use crate::core::socket_handler::SocketHandler;
+use crate::{core::socket_handler::SocketHandler, api::schema::instructions::CoreInstruction};
 
 use std::{
     process::{Child, Command, Stdio},
-    fmt::Debug, path::PathBuf
+    fmt::Debug, path::PathBuf,
+    sync::mpsc::{self, Receiver}
 };
-use log::{warn, debug, error};
+use log::{warn, debug, error, trace};
 
 use anyhow::Result;
+use tokio::task::JoinHandle;
 
 #[derive(Debug)]
 pub struct Process {
     child: Child,
-    socket: SocketHandler,
-    process_path: PathBuf
+    process_path: PathBuf,
+    core_read_thread: JoinHandle<()>,
+    rx: Receiver<Result<CoreInstruction>>
 }
 
 impl Process {
-    pub fn new<T>(path: T, socket: SocketHandler) -> Result<Process> where PathBuf: From<T> {
+    pub fn new<T>(path: T, mut socket:SocketHandler) -> Result<Process> where PathBuf: From<T> {
         let path = PathBuf::from(path);
+        let (tx, rx) = mpsc::channel();
 
         match Command::new(&path).stdout(Stdio::null()).spawn() {
             Ok(child) => {
                 debug!("Successfully started process {:?} with PID {}", path, child.id());
                 Ok(Process {
-                    child: child,
-                    socket,
-                    process_path: path
+                    child,
+                    core_read_thread: tokio::spawn(async move {
+                        loop {
+                            let data = socket.get_instruction().await;
+                            match &data {
+                                Ok(v) => {
+                                    trace!("Sent result {}", v);
+                                }
+                                Err(e) => {
+                                    trace!("Sent error {}", e);
+                                }
+                            };
+                            match tx.send(data) {
+                                Err(e) => {
+                                    error!("Could not send instruction {}", e);
+                                }
+                                _ => {}
+                            };
+                        }
+                    }),
+                    process_path: path,
+                    rx      
                 })
             },
             Err(e) => {
@@ -34,24 +57,51 @@ impl Process {
             }
         }
     }
-    pub async fn get_instruction(&self) {
+
+    /**
+     * Returns if the next core instruction is available
+     * 
+     * # Returns
+     * A [bool] on if the next instruction is available
+     **/
+    pub fn poll_next_instruction(&self) -> Option<Result<CoreInstruction>> {
+        match self.rx.try_recv() {
+            Ok(v) => Some(v),
+            Err(e) => {
+                match e {
+                    mpsc::TryRecvError::Empty => None,
+                    mpsc::TryRecvError::Disconnected => {
+                        error!("Sending channel for {} disconnected, this should NEVER happen", self.process_path.display());
+                        Some(Err(e.into()))
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn get_next_instruction(&mut self) -> Result<CoreInstruction> {
+        match self.rx.recv() {
+            Ok(v) => v,
+            Err(e) => Err(e.into())
+        }
     }
 }
 
 impl Drop for Process {
     fn drop(&mut self) {
         let id = self.child.id();
+        self.core_read_thread.abort();
         // TODO: Handle the case where the process terminates early, either
         // by checking if it's still running, or gracefully failing on kill.
         // On Windows, if it terminates early, it's an access denied error.
         match self.child.try_wait() {
             Ok(Some(status)) => {
-                debug!("Process exited early: code {}", status);
+                debug!("Process {} exited early: code {}", self.process_path.display(), status);
                 return;
             },
             Ok(None) => {}, // Process still running
             Err(e) => {
-                error!("Error checking state of process, reason: {}", e);
+                error!("Error checking state of process {}, reason: {}", self.process_path.display(), e);
                 return;
             }
         };
